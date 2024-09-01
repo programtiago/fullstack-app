5bf33e2aa3b07ff8fdd9b26590318cf53e0431a0
Tiago Silva <programtiago@gmail.com>
2024-09-01 18:30:19 +0100

    Environment for Angular and Spring

---
+++ backend/src/test/java/com/dev/tiago/fullstack_app/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.tiago.fullstack_app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
